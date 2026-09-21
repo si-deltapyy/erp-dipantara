a91fe7637ffedcd73240b39b89c2969d72d60679
Sangaji Suryo <sguritno16@student.uns.ac.id>
2026-09-21 12:44:32 +0700

    configure all

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,6 @@ use App\Http\Controllers\SpkController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Landing', [
-        'title' => 'Dipantara - Sistem Manajemen Pesanan & Keuangan Kayu',
-        'message' => 'Selamat datang di Dipantara, sistem manajemen pesanan dan keuangan kayu yang membantu bisnis kayu mengelola stok, pesanan, dan keuangan secara efisien.',
-    ]);
-});
-
 Route::get('/dashboard',[Dashboard::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
